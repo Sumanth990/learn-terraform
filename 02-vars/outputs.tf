9df4e599c726dd8afd36f44db166b820418c097f
Sumanth <9ssumanth@gmail.com>
2024-02-11 17:28:52 +0530

    variable index and map example

--- 02-vars/outputs.tf
+++ 02-vars/outputs.tf
@@ -1,3 +1,16 @@
 output "v1" {
   value = var.v1
 }
+
+output "First_value_in_list" {
+  value = var.v2[0]   #index number starts from 0
+}
+
+output "Second_value_in_list" {
+  value = var.v2[1]
+}
+
+#Value in a map
+output "value_in_map" {
+  value = var.v3["abc"] #provide the key "abc"
+}
--- 02-vars/variables.tf
+++ 02-vars/variables.tf
@@ -1,3 +1,18 @@
 variable "v1" {
   default = "Hello World"
 }
+
+# A variable list can have different data types like string,number,boolean.
+
+variable "v2" {
+  default = [ "Hello world", 100, true]
+}
+
+# Map - A variable having multiple keys and values.
+
+variable "v3" {
+  default = {
+    abc = 100
+    xyz = "Two Hundred"
+  }
+}
