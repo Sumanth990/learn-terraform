424a13b28fc88fce0023d0d9f8d54502c15081c4
Sumanth <9ssumanth@gmail.com>
2024-02-12 19:49:29 +0530

    Creating 3 instances backend/frontend/mysql

--- expense-v1/main.tf
+++ expense-v1/main.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "Frontend" {
   ami                    = "ami-0f3c7d07486cad139"
-  vpc_security_group_ids = "sg-04683e9af944ef9c6"
+  vpc_security_group_ids = ["sg-04683e9af944ef9c6"]
   instance_type          = "t3.micro"
                 #Terraform standards - separate block separate with empty line, also maintains equals in same level
   tags = {
@@ -10,7 +10,7 @@ resource "aws_instance" "Frontend" {
 
 resource "aws_instance" "Backend" {
   ami                    = "ami-0f3c7d07486cad139"
-  vpc_security_group_ids = "sg-04683e9af944ef9c6"
+  vpc_security_group_ids = ["sg-04683e9af944ef9c6"]
   instance_type          = "t3.micro"
   #Terraform standards - separate block separate with empty line, also maintains equals in same level
   tags = {
@@ -20,7 +20,7 @@ resource "aws_instance" "Backend" {
 
 resource "aws_instance" "MySQL" {
   ami                    = "ami-0f3c7d07486cad139"
-  vpc_security_group_ids = "sg-04683e9af944ef9c6"
+  vpc_security_group_ids = ["sg-04683e9af944ef9c6"]
   instance_type          = "t3.micro"
   #Terraform standards - separate block separate with empty line, also maintains equals in same level
   tags = {
