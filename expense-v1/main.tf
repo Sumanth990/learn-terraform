23abd37d0a3e2231d6d4d3b67175f91c5cefa3de
Sumanth <9ssumanth@gmail.com>
2024-02-12 19:53:41 +0530

    Creating 3 instances backend/frontend/mysql

--- expense-v1/main.tf
+++ expense-v1/main.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "Frontend" {
   ami                    = "ami-0f3c7d07486cad139"
   vpc_security_group_ids = ["sg-04683e9af944ef9c6"]
   instance_type          = "t3.micro"
-                #Terraform standards - separate block separate with empty line, also maintains equals in same level
+  #Terraform standards - separate block separate with empty line, also maintains equals in same level
   tags = {
     Name = "Frontend-dev"
   }
