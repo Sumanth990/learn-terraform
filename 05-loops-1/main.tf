b6d74bfea493f7aabcbafffc31fc9080c6d317e8
Sumanth <9ssumanth@gmail.com>
2024-02-28 19:38:38 +0530

    creating expense project instances using element

--- 05-loops-1/main.tf
+++ 05-loops-1/main.tf
@@ -8,13 +8,28 @@ output "test" {
   value = null_resource.test.*.id
 }
 
+#resource "aws_instance" "test" {
+#  count                  = 3
+#  ami                    = "ami-0f3c7d07486cad139"
+#  instance_type          = "t3.micro"
+#  vpc_security_group_ids = ["sg-020e23d79f8100a7e"]
+#
+#  tags = {
+#    Name = "test-${count.index}"
+#  }
+#}
+
+variable "components" {
+  default = ["frontend", "backend", "mysql"]
+}
+
 resource "aws_instance" "test" {
-  count                  = 3
+  count                  = length(var.components)
   ami                    = "ami-0f3c7d07486cad139"
   instance_type          = "t3.micro"
   vpc_security_group_ids = ["sg-020e23d79f8100a7e"]
 
   tags = {
-    Name = "test-${count.index}"
+    Name = element(var.components,count.index)
   }
 }
