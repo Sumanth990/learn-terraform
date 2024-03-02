1432a02d5a14a6f3ca52db69b7f7e86a0594665e
Sumanth <9ssumanth@gmail.com>
2024-03-02 17:51:45 +0530

    creating instance using for_each and lookup

--- expense-v4/main.tf
+++ expense-v4/main.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "instances" {
   for_each               = var.components
   ami                    = data.aws_ami.centos08.image_id
   vpc_security_group_ids = var.vpc_security_group_ids
-  instance_type          = lookup(var.components,"instance_type", "t2.micro")
+  instance_type          = lookup(each.value,"instance_type", "t2.micro")
   #Terraform standards - separate block separate with empty line, also maintains equals in same level
   tags = {
     Name = lookup(each.value, "name", null)
@@ -12,7 +12,7 @@ resource "aws_instance" "instances" {
 resource "aws_route53_record" "records" {
   for_each = var.components
   zone_id  = var.zone_id
-  name     = lookup(var.components, "name", null)
+  name     = lookup(each.value, "name", null)
   type     = "A"
   ttl      = 30
   records  = [lookup(lookup(aws_instance.instances,each.key, null), "private_ip", null)]
