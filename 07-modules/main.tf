5abe245a48511c348a328771835f1e55fce2dfde
Sumanth <9ssumanth@gmail.com>
2024-03-25 21:22:13 +0530

    creating db_subnet, aws security group...

--- 07-modules/main.tf
+++ 07-modules/main.tf
@@ -1,8 +1,8 @@
-module "ec2" {
+module "ec2" {                                  #module1
   source = "./modules/ec2"
 }
 
-module "route53" {
+module "route53" {                              #module2
   source = "./modules/route53"
   private_ip = module.ec2.private_ip
 }
