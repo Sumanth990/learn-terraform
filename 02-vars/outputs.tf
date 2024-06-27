1384a6ffa0bb945e2cd09a0ede3b184018edb9b2
Sumanth <9ssumanth@gmail.com>
2024-06-27 21:56:44 +0530

    variables lookup

--- 02-vars/outputs.tf
+++ 02-vars/outputs.tf
@@ -1,23 +1,27 @@
-output "v1" {
-  value = var.v1
-}
-
-output "First_value_in_list" {
-  value = var.v2[0]
-}
-
-output "Second_value_in_list" {
-  value = var.v2[1]
-}
-
-output "value_in_map" {
-  value = var.v3["abc"]
-}
+#output "v1" {
+#  value = var.v1
+#}
+#
+#output "First_value_in_list" {
+#  value = var.v2[0]
+#}
+#
+#output "Second_value_in_list" {
+#  value = var.v2[1]
+#}
+#
+#output "value_in_map" {
+#  value = var.v3["abc"]
+#}
 
 #output "value_in_map1" {
 #  value = var.v3["abcd"]
 #}
 
-output "value_in_map2" {
-  value = lookup(var.v3,"abcd", "what?")
+#output "value_in_map2" {
+#  value = lookup(var.v3,"abcd", "what?")
+#}
+
+output "Second_value_in_lists" {
+  value = element(var.v3, 1)
 }
