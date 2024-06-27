12eabacdcaec94db45ad6c72f21d437190a65a94
Sumanth <9ssumanth@gmail.com>
2024-06-27 21:42:09 +0530

    variables

--- 02-vars/outputs.tf
+++ 02-vars/outputs.tf
@@ -1,3 +1,7 @@
 output "v1" {
   value = var.v1
 }
+
+output "v2" {
+  value = var.v2
+}
--- 02-vars/variables.tf
+++ 02-vars/variables.tf
@@ -1,3 +1,7 @@
 variable "v1" {
   default = "Hello world"
 }
+
+variable "v2" {
+  default = ["Hello world", 100, true]
+}
