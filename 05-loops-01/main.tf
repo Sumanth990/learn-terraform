5b706c351632ddad61fddf7251b67f943d8c22a4
Sumanth <9ssumanth@gmail.com>
2024-06-28 11:51:55 +0530

    count create instance

--- 05-loops-01/main.tf
+++ 05-loops-01/main.tf
@@ -9,3 +9,10 @@ output "test" {
 output "test2" {
   value = null_resource.test.*
 }
+
+resource "aws_instance" "test" {
+  count                  = 3
+  ami                    = "ami-0b4f379183e5706b9"
+  instance_type          = "t3.micro"
+  vpc_security_group_ids = ["sg-05eadccf3fd7c62d1"]
+}
