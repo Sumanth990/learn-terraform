cbff7b17d78a007805a0a87608f536b98bf995dd
Sumanth <9ssumanth@gmail.com>
2024-06-28 17:30:32 +0530

    for_each - instance and route_53 creation

--- expense-v4/main.tf
+++ expense-v4/main.tf
@@ -5,14 +5,14 @@ resource "aws_instance" "instances" {
   vpc_security_group_ids = [var.vpc_security_group_ids]
 
   tags = {
-    Name = "${lookup(var.components, "name", null)}-dev"
+    Name = "${lookup(each.value, "name", null)}-dev"
   }
 }
 
 resource "aws_route53_record" "www" {
   for_each = var.components
   zone_id  = var.zone_id
-  name     = "${lookup(var.components, "name", null)}-dev"
+  name     = "${lookup(each.value, "name", null)}-dev"
   type     = "A"
   ttl      = 30
   records  = [lookup(lookup(aws_instance.instances, each.key, null), "private_ip", null)]
