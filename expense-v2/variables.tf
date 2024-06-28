af8729a65f9e16b5ac9b22a701e9b88502945272
Sumanth <9ssumanth@gmail.com>
2024-06-28 09:14:21 +0530

    expense-v2

---
+++ expense-v2/variables.tf
@@ -0,0 +1,11 @@
+variable "instance_type" {
+  default = "t3.micro"
+}
+
+variable "vpc_security_group_ids" {
+  default = "sg-05eadccf3fd7c62d1"
+}
+
+variable "zone_id" {
+  default = "Z0531898YLDJ6DDU67F4"
+}
