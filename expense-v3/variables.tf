53c75fd787a733cb02919bf58bc20b04c437b380
Sumanth <9ssumanth@gmail.com>
2024-06-28 12:11:56 +0530

    count instance creation

---
+++ expense-v3/variables.tf
@@ -0,0 +1,15 @@
+variable "instance_type" {
+  default = "t3.micro"
+}
+
+variable "vpc_security_group_ids" {
+  default = "sg-05eadccf3fd7c62d1"
+}
+
+variable "components" {
+  default = ["frontend", "backend", "mysql"]
+}
+
+variable "zone_id" {
+  default = "Z0531898YLDJ6DDU67F4"
+}
