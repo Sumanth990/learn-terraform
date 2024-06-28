250a16a73b6032b0e09c7c9fa038dbfde729c5ed
Sumanth <9ssumanth@gmail.com>
2024-06-28 13:18:12 +0530

    for_each

--- 05-loops-2/main.tf
+++ 05-loops-2/main.tf
@@ -1,16 +1,16 @@
-variable "fruit1" {
-  default = {
-    apple = 10
-    banana = 100
-  }
-}
-
-resource "null_resource" "test1" {
-  for_each = var.fruit1
-  provisioner "local-exec" {
-    command = "echo ${each.key}= ${each.value}"
-  }
-}
+#variable "fruit1" {
+#  default = {
+#    apple = 10
+#    banana = 100
+#  }
+#}
+#
+#resource "null_resource" "test1" {
+#  for_each = var.fruit1
+#  provisioner "local-exec" {
+#    command = "echo ${each.key}= ${each.value}"
+#  }
+#}
 
 variable "fruit2" {
   default = {
